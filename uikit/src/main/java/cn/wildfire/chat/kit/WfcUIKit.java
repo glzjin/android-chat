53920d81b4095cbd7479c9ab6fc63f09eba1bbbd
glzjin <glzjin2015@gmail.com>
2024-10-17 23:15:55 +0800

    Update WfcUIKit.java

--- uikit/src/main/java/cn/wildfire/chat/kit/WfcUIKit.java
+++ uikit/src/main/java/cn/wildfire/chat/kit/WfcUIKit.java
@@ -8,7 +8,7 @@ import android.app.Activity;
 import android.app.Application;
 import android.app.PendingIntent;
 import android.content.Context;
-import android.content.Intent;Set
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.media.AudioManager;
 import android.net.Uri;
